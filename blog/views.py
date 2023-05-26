ad53a7ac7d3df5a1a40252c3ae860b679b8d560c
viktor <vktr.buyukli@gmail.com>
2023-05-26 12:29:45 +0300

    update index

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,12 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
+from blog.services import get_blogs
 
 
 def index(request):
-    return render(request, "index.html")
+    blogs = get_blogs()
+    context = {
+        "blogs": blogs,
+    }
+    return render(request, "index.html", context)
