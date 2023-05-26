75ea07d64cfaf026e8584165b946792481fbeebc
viktor <vktr.buyukli@gmail.com>
2023-05-26 12:38:07 +0300

    add views

---
+++ blog/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from django.core.exceptions import ValidationError
+
+from blog.services import get_blogs
+
+
+class BlogForm(forms.Form):
+    blog_id = forms.IntegerField(label="Blog number (id)", min_value=1)
+
+    def clean_blog_id(self):
+        blog_id = self.cleaned_data["blog_id"]
+        if str(blog_id) not in get_blogs():
+            raise ValidationError("This blog does not exist. Available numbers below")
+        return blog_id
--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("blog/", views.get_blog, name="blog"),
+    path("blog/<int:blog_id>/", views.blog_view, name="blog_view"),
 ]
